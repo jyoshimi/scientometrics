76a92f3fa83c047a75f2f05ab8ba665afbc37cd4
Jeff Yoshimi <jyoshimi@ucmerced.edu>
2020-06-08 10:54:28 -0700

    Mostly code comment changes and notes to Pablo in the data reading script

--- citation_matrix_WOS.R
+++ citation_matrix_WOS.R
@@ -4,31 +4,26 @@ library('tidyverse')
 
 # Load and parse articles ------
 
-# In file: cited authors are the authors that receive the citations. 
-# citing authors are the authors that DO the citations.
-
-
-files <- readFiles('data/WoS/citations-1-500.txt','data/WoS/citations-501-1000.txt', 
-                   'data/WoS/citations-1001-1500.txt', 'data/WoS/citations-1501-2000.txt', 
-                   'data/WoS/citations-2001-2500.txt', 'data/WoS/citations-2501-3000.txt',
-                   'data/WoS/citations-3001-3500.txt', 'data/WoS/citations-3501-4000.txt',
-                   'data/WoS/citations-4001-4500.txt','data/WoS/citations-4501-4543.txt')
-
-# Load raw.articles. View it to see main data.  
-# AU is main authors of articles
+# Load raw.articles. View it to see main data. 
+# AU is citing author
 # CR is citations in the article
-# CR_AU is first author listed in each citation
+# CR_AU is first author listed in each citation.  I..e cited author
 citations_file = "raw_articles.rds"
 if (file.exists(citations_file)) {
   raw.articles <- read_rds(citations_file)
 } else {
+  # Pablo: this is not working with current libraries. Can you delete raw_articles.rds and then see if this works or what update is needed?
+  files <- readFiles('data/WoS/citations-1-500.txt','data/WoS/citations-501-1000.txt', 
+                     'data/WoS/citations-1001-1500.txt', 'data/WoS/citations-1501-2000.txt', 
+                     'data/WoS/citations-2001-2500.txt', 'data/WoS/citations-2501-3000.txt',
+                     'data/WoS/citations-3001-3500.txt', 'data/WoS/citations-3501-4000.txt',
+                     'data/WoS/citations-4001-4500.txt','data/WoS/citations-4501-4543.txt')
   raw.articles <- convert2df(files,dbsource = "isi", format = "plaintext")
   write_rds(raw.articles, citations_file)
 }
 
 # Pulls the cited authors from CR and adds that CR_AU to a new table
 parsed.articles <- metaTagExtraction(M = raw.articles, Field = "CR_AU")
-
 
 # Create and consolidate citation matrix ------
 
@@ -37,7 +32,7 @@ parsed.articles <- metaTagExtraction(M = raw.articles, Field = "CR_AU")
 citing.matrix.raw <- cocMatrix(parsed.articles, Field = "CR_AU", type = "matrix", sep = ";") %>% 
   as_tibble()
 
-# Useful code snippets (focused only on citing authors)  ------
+# Useful code snippets ------
 # write(sort(unique(colnames(citing.matrix.raw))), file = "~/Desktop/citedAuthors.txt")
 
 # See number of articles grabbed from each journal
@@ -61,7 +56,6 @@ citing.matrix.raw <- cocMatrix(parsed.articles, Field = "CR_AU", type = "matrix"
 # Wrangle the data -----
 
 # Parse out the original authors (AU) and save only the first author in multi-authored pieces
-
 first.author <- map_chr(strsplit(parsed.articles$AU, ';'), function(x){return(x[1])})
 
 # Add first author as first column
@@ -69,6 +63,7 @@ citing.matrix.raw <- citing.matrix.raw %>%
   mutate(first.author) %>%    
   select(first.author, everything())    # Re-order so first.author is first column
 
+# Pablo: Shouldn't shorten.name be moved above this?
 all.names <- c(colnames(citing.matrix.raw), citing.matrix.raw$first.author) %>% 
   unique() %>% 
   enframe() %>% 
@@ -79,6 +74,7 @@ all.names <- c(colnames(citing.matrix.raw), citing.matrix.raw$first.author) %>%
   filter(str_detect(old.name, "[[:digit:]]", negate = TRUE))
 write_csv(all.names, "current_names.csv")
 
+# Pablo, can the next commented out blocks be deleted?
 # first.authors.new <- map_chr(citing.matrix.raw$first.author, function(name){
 #   if(name %in% modified.authors$old.name){
 #     name.ind <- which(modified.authors$old.name == name)
@@ -101,21 +97,18 @@ citing.matrix.clean <- citing.matrix.raw %>%
   filter(first.author != "NA", !str_detect(first.author,"ANONYMOUS"), !(is.na(first.author))) %>% 
   # Removing columns with numbers
   select(-matches("[[:digit:]]"))
-
-# 
-# 
-# %>%
-#   # Remove NA and Anonymous columns
-#   select(-ANONYMOUS, -`NA`) %>%                           
-#   # Remove specific columns
-#   select(-`A CORRECTION`, -`A LUDW U FREIB HA`, -`AA VV`, -`AA XV`, -`AAVV`) %>% 
-#   select(-`HUA ZHENGREN`, -`HUA DOK`) %>% # Remove confounds for Husserliana volumes. These authors have only 1 citation, so they'll get deleted anyway.
-#   # Removing columns with numbers
   
-
+# Snippet to create a list of cited authors
 # write(unlist(unique(citing.matrix.clean[,"first.author"]),use.names = FALSE), 
 #       file = "~/Desktop/mainAuthors.txt")
 # write(sort(unique(colnames(citing.matrix.clean))), file = "~/Desktop/citedAuthors.txt")
+
+# Pablo: can the next line be removed?
+# reference.names <- read_csv('../scientometrics/temp_fixed_authors.csv', col_names = c("AU", "new.name"))
+
+# Pablo: add brief comment here? 
+modified.authors <- read_csv(file = "name_changes.csv") %>% 
+  select(old.name = `OLD NAME`, new.name = `NEW NAME ("NA" to Delete)`)
 
 # Create shortened names: e.g. HEIDEGGER MARTIN -> HEIDEGGER M Can do
 # hand-wrangling to associate one regex of names with a shortened name TODO:
@@ -123,13 +116,6 @@ citing.matrix.clean <- citing.matrix.raw %>%
 # name and first initial TODO: DE P, DA SILVA, AL names TODO: Consider authors
 # cited only by last name. e.g. HEIDEGGER, ADORNO, etc. Should be turned into
 # complete name by hand?
-
-
-# reference.names <- read_csv('../scientometrics/temp_fixed_authors.csv', col_names = c("AU", "new.name"))
-
-modified.authors <- read_csv(file = "name_changes.csv") %>% 
-  select(old.name = `OLD NAME`, new.name = `NEW NAME ("NA" to Delete)`)
-
 shorten.name <- function(x) {
   # Input:
   #       `x` is a string with a full name of an author, ideally in the form LASTNAME FIRSTNAME
@@ -147,44 +133,8 @@ shorten.name <- function(x) {
     print(modified.authors$new.name[name.ind])
     return(modified.authors$new.name[name.ind])
   }
-  # if(x %in% reference.names$AU){
-  #   reference.ind <- which(reference.names$AU == x)
-  #   # print(paste("Before:", x))
-  #   # print(paste("After:", reference.names$new.name[reference.ind]))
-  #   return(reference.names$new.name[reference.ind])
-  # }
-  # if (str_detect(x, "ARISTO[A-Z]+[[:space:]]*")) {
-  #   return("ARISTOTLE")
-  # }
-  # # Now: HUSSERL and HUSSERLIANA
-  # else if (str_detect(x, "HUSSER[A-Z]")) {
-  #   # Detects almost all instances of Husserl appearing
-  #   return("HUSSERL E")
-  # } else if (str_detect(x, "^HUA$")) {
-  #   return("HUSSERL E")
-  # } else if (str_detect(x, "^HUA[[:space:]]")) {
-  #   return("HUSSERL E")
-  # } else if (str_detect(x,"HUSERL")) {
-  #   return ("HUSSERL E")
-  # }
-  # 
-  # if (str_detect(x, "^MERLEAU.*")) {
-  #   return ("MERLEAUPONTY M")
-  # }
-  # 
-  # # NOW: NAMES WITH SPACES
-  # if (str_detect(x, "^VAN DER|^VAN DEN|^VON DER")) {
-  #   return(
-  #     str_extract(
-  #       x,
-  #       "^[A-Z]+[[:space:]][A-Z]+[[:space:]][A-Z]+[[:space:]]*[[A-Z]]{1}"
-  #     )
-  #   )
-  # } else if (str_detect(x, "^VAN |^VON ")) {
-  #   return(str_extract(x, "^[A-Z]+[[:space:]][A-Z]+[[:space:]]*[[A-Z]]{1}"))
-  # } 
 else{
-    # The main algorithm applied to everyone. Special cases above
+    # The main algorithm applied to everyone. Special cases are above.
     # Extracts the longest string followed by a space and then the
     # first letter of the next string
     return(str_extract(x, "^[A-Z]+[[:space:]]*[[A-Z]]{1}"))
@@ -206,7 +156,7 @@ citing.matrix.clean <- citing.matrix.clean %>%
   filter(!(is.na(first.author)),
          first.author != "NA") 
 
-# For inspection
+# Snippet to inspect shortened names
 # write(sort(unique(short.names)), file = "~/Desktop/shortnames.txt")
 
 # Consolidate redundant rows introduced by shortening names
@@ -215,7 +165,7 @@ citing.matrix.clean <- citing.matrix.clean %>%
   group_by(first.author) %>% 
   summarise_all(sum)
 
-# Pre-allocating a new matrix with consolidated columns
+# Pre-allocate a new matrix with consolidated columns.
 # Number of rows is same as citing matrix. 
 # Number of columns is number of unique cited authors
 new.citing.matrix <- matrix(
@@ -229,8 +179,6 @@ new.citing.matrix <- matrix(
 temp.citing.matrix <- citing.matrix.clean[,-1] %>% 
   as.matrix()
 row.names(temp.citing.matrix) <- citing.matrix.clean$first.author
-
-
 
 # Now consolidate the COLUMNS of the citing matrix
 # Manully does for columns what the row consolidation above does for row
@@ -249,29 +197,17 @@ for(name in sort(colnames(citing.matrix.clean)[-1])){
     }
 }
 
-# For inspection (Is ths right thing to look at?)
+# Snippets for inspection
 # write(sort(unique(rownames(new.citing.matrix))), file = "~/Desktop/new_mainAuthors.txt")
 # write(sort(unique(colnames(new.citing.matrix))), file = "~/Desktop/new_citedAuthors.txt")
 
-# Manually fix some authors (doing regex makes this extremely slow)
-# TODO: This is where some hand work should be done.  Are there other cases of this.
-# TODO: First try to change shorten.name above so this is not needed
-# Pablo: I FIXED ARISTOTLE
-
-# aristotle.columns <- colnames(new.citing.matrix) %>% .[str_detect(., "ARISTOT") & !(str_detect(., " "))] 
-# new.aristotle.column <- apply(X = new.citing.matrix[,aristotle.columns], MARGIN = 1, FUN = sum)
-# new.citing.matrix <- new.citing.matrix[, !(colnames(new.citing.matrix) %in% aristotle.columns)]
-# new.citing.matrix <- cbind(new.citing.matrix, new.aristotle.column)
-# colnames(new.citing.matrix)[ncol(new.citing.matrix)] <- "ARISTOTLE"
-
-# Cleanup: Remove any cited author with less than 1 
-# citation (that is, only 1 citing author cited them only 1 time)
+# Cleanup: Remove any cited author with less than 1 citation
+# (that is, only 1 citing author cited them only 1 time)
 new.citing.matrix <- new.citing.matrix[, colSums(new.citing.matrix) > 1]
 new.citing.matrix <- new.citing.matrix[,sort(colnames(new.citing.matrix))]
 
 # Create small matrix ------
 
-# Create "Small matrix"
 small.threshold <- 5
 
 # For the small matrix, remove cited authors with less than small.threshold citations
@@ -280,9 +216,7 @@ small.citing.matrix <- new.citing.matrix[, colSums(new.citing.matrix) >= small.t
 # That is, citing authors with only 0 now in their whole row
 small.citing.matrix <- small.citing.matrix[rowSums(small.citing.matrix) >= 0,]
 
-
 # Save matrices ------
-
 
 # Transform both matrices into sparse for backup saving 
 # This uses the Matrix package
@@ -300,7 +234,6 @@ Matrix::writeMM(small.citing.matrix, "small_matrix.txt")
 
 
 # Make edge lists ------
-
 
 # Now make the edge list for the network.
 # This is a directed edge list, so it's a "long" (vs "wide") version of the matrix.
